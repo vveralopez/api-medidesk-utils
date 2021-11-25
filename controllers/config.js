cccc35ca1797e4a94c1edb6671c9bd05be0165b6
Victor Vera <victor.veralopez@hotmail.com>
2021-11-24 23:14:54 -0300

    Ajustes Puertos

--- controllers/config.js
+++ controllers/config.js
@@ -8,8 +8,8 @@ const limpiaBase64 = (data) => {
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     HOST: process.env.HOST || 'localhost',
-    PORT: limpiaBase64(process.env.PORT || 'MzAwOQo='),
-    PGPORT: limpiaBase64(process.env.PGPORT || 'NTQzNAo='), // )   'NTQzMgo=' 
+    PORT: parseInt(limpiaBase64(process.env.PORT || 'MzAwOQo=')),
+    PGPORT: parseInt(limpiaBase64(process.env.PGPORT || 'NTQzNAo=')), // )   'NTQzMgo=' 
     PGHOSTNAME: limpiaBase64(process.env.PGHOSTNAME || 'MTYyLjIxMi4xNTMuMjI1Cg=='), // 'bG9jYWxob3N0Cg==' ), //--ip vps'MTYyLjIxMi4xNTMuMjI1Cg=='),
     PGUSERLOGIN: limpiaBase64(process.env.PGUSERLOGIN || 'bG9naW4K') ,
     PGUSERLOGPW: limpiaBase64(process.env.PGUSERLOGPW || 'U29maWFWaWN0b3JpYTIwMjAwMzEzCg=='),
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { PORT } = require('./controllers/config');
+const { HOST, PORT } = require('./controllers/config');
 
 //Middleware
 app.use(bodyParser.urlencoded({extended: false}));
@@ -20,6 +20,6 @@ app.use(cors());
 //Routes
 app.use(require('./routes/index'))
 
-app.listen(parseInt(PORT), () => {
+app.listen(PORT, () => {
   console.log('Servidor montado en puerto: ' + PORT);
 });

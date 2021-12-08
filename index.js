ce1cd39ce3ff0bf87f0f09a50df3b84ee8b95447
Victor Vera <victor.vera@it-trust.cl>
2021-12-07 22:48:29 -0300

    Ajustes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+
 //Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());

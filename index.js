119e0f4cc3abdfe48be3a7a49aae578aeb8ddb95
vveralopez <78865201+vveralopez@users.noreply.github.com>
2021-12-09 20:22:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-
 //Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
